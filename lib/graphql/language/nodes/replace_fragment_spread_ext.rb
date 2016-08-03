7114da42719c399f87deb013dc1dde0b8a7dc9be
Joshua Peek <josh@joshpeek.com>
2016-08-03 16:59:48 -0700

    Add helper for replacing fragment spreads

---
+++ lib/graphql/language/nodes/replace_fragment_spread_ext.rb
@@ -0,0 +1,44 @@
+require "graphql"
+require "graphql/language/nodes/selection_ext"
+
+module GraphQL
+  module Language
+    module Nodes
+      module Selections
+        def replace_fragment_spread(fragments)
+          other = self.dup
+          other.selections = self.selections.map do |selection|
+            case selection
+            when FragmentSpread
+              if fragment = fragments[selection.name.to_sym]
+                GraphQL::Language::Nodes::InlineFragment.new(type: fragment.type, directives: fragment.directives, selections: fragment.selections)
+              else
+                selection
+              end
+            when GraphQL::Language::Nodes::Selections
+              selection.replace_fragment_spread(fragments)
+            else
+              selection
+            end
+          end
+          other
+        end
+      end
+
+      class Document < AbstractNode
+        def replace_fragment_spread(fragments)
+          other = self.dup
+          other.definitions = self.definitions.map do |definition|
+            case definition
+            when GraphQL::Language::Nodes::Selections
+              definition.replace_fragment_spread(fragments)
+            else
+              definition
+            end
+          end
+          other
+        end
+      end
+    end
+  end
+end
---
+++ test/test_replace_fragment_spread_ext.rb
@@ -0,0 +1,96 @@
+require "graphql"
+require "graphql/language/nodes/deep_freeze_ext"
+require "graphql/language/nodes/replace_fragment_spread_ext"
+require "minitest/autorun"
+
+class TestReplaceFragmentSpreadExt < MiniTest::Test
+  def test_query_replace_fragment_spread
+    document = GraphQL.parse(<<-'GRAPHQL').deep_freeze
+      query FooQuery {
+        node(id: "4") {
+          id
+          ... UserFragment
+          ... MissingFragment
+        }
+      }
+
+      fragment UserFragment on User {
+        name
+        friends {
+          name
+        }
+      }
+    GRAPHQL
+
+    query, fragment = document.definitions
+
+    new_query = query.replace_fragment_spread({
+      :"UserFragment" => fragment
+    })
+    refute new_query.frozen?
+
+    expected = <<-'GRAPHQL'
+      query FooQuery {
+        node(id: "4") {
+          id
+          ... on User {
+            name
+            friends {
+              name
+            }
+          }
+          ... MissingFragment
+        }
+      }
+    GRAPHQL
+    assert_equal expected.gsub(/^      /, "").chomp, GraphQL::Language::Nodes::Document.new(definitions: [new_query]).to_query_string
+  end
+
+  def test_document_replace_fragment_spread
+    document = GraphQL.parse(<<-'GRAPHQL').deep_freeze
+      query FooQuery {
+        node(id: "4") {
+          id
+          ... UserFragment
+          ... MissingFragment
+        }
+      }
+
+      fragment UserFragment on User {
+        name
+        friends {
+          name
+        }
+      }
+    GRAPHQL
+
+    _, fragment = document.definitions
+
+    new_document = document.replace_fragment_spread({
+      :"UserFragment" => fragment
+    })
+    refute new_document.frozen?
+
+    expected = <<-'GRAPHQL'
+      query FooQuery {
+        node(id: "4") {
+          id
+          ... on User {
+            name
+            friends {
+              name
+            }
+          }
+          ... MissingFragment
+        }
+      }
+      fragment UserFragment on User {
+        name
+        friends {
+          name
+        }
+      }
+    GRAPHQL
+    assert_equal expected.gsub(/^      /, "").chomp, new_document.to_query_string
+  end
+end
