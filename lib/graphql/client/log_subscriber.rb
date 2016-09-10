1ffa8dc6ebbe5b711f231546fa8096834d1ce27d
Joshua Peek <josh@joshpeek.com>
2016-09-10 23:33:00 +0900

    Add log subscriber

---
+++ lib/graphql/client/log_subscriber.rb
@@ -0,0 +1,14 @@
+require "active_support/log_subscriber"
+
+module GraphQL
+  class Client
+    class LogSubscriber < ActiveSupport::LogSubscriber
+      def query(event)
+        info { "#{event.payload[:name]} (#{event.duration}) #{event.payload[:operation_name].gsub("__", "::")}" }
+        debug { event.payload[:document].to_query_string }
+      end
+    end
+  end
+end
+
+GraphQL::Client::LogSubscriber.attach_to :graphql
