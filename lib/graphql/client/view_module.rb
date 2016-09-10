5b2190bf061f8c90328d8ec0ee17a65d4be71054
Joshua Peek <josh@joshpeek.com>
2016-09-10 22:19:55 +0900

    Add view module loader

---
+++ lib/graphql/client/view_module.rb
@@ -0,0 +1,145 @@
+require "active_support/dependencies"
+require "active_support/inflector"
+require "graphql/client/erubis"
+
+module GraphQL
+  class Client
+    module ViewModule
+      attr_accessor :client
+
+      # Public: Eager load module and all subdependencies.
+      #
+      # Use in production when cache_classes is true.
+      #
+      # Traverses all app/views/**/*.erb and loads all static constants defined in
+      # ERB files.
+      #
+      # Examples
+      #
+      #   Views.eager_load!
+      #
+      # Returns nothing.
+      def eager_load!
+        return unless File.directory?(self.path)
+
+        Dir.entries(self.path).each do |entry|
+          next if entry == "." || entry == ".."
+          name = entry.sub(/(\.\w+)+$/, "").camelize.to_sym
+          if ViewModule.valid_constant_name?(name) && loadable_const_defined?(name)
+            mod = const_get(name, false)
+            mod.eager_load!
+          else
+            # Ignore template names that don't camelize to safe constants:
+            #   404, foo-dash
+          end
+        end
+
+        nil
+      end
+
+      # Internal: Check if name is a valid Ruby constant identifier.
+      #
+      # name - String or Symbol constant name
+      #
+      # Examples
+      #
+      #   valid_constant_name?("Foo") #=> true
+      #   valid_constant_name?("404") #=> false
+      #
+      # Returns true if name is a valid constant, otherwise false if name would
+      # result in a "NameError: wrong constant name".
+      def self.valid_constant_name?(name)
+        name.to_s =~ /^[A-Z][a-zA-Z0-9_]*$/
+      end
+
+      # Public: Override constant defined to check if constant name matches a
+      # view directory or template namespace.
+      #
+      # name - String or Symbol constant name
+      # inherit - If the lookup will also search the ancestors (default: true)
+      #
+      # Returns true if definition is found, otherwise false.
+      # def const_defined?(name, inherit = true)
+      #   if super(name.to_sym, inherit)
+      #     true
+      #   elsif const_path(name)
+      #     true
+      #   else
+      #     false
+      #   end
+      # end
+
+      def loadable_const_defined?(name)
+        if const_defined?(name.to_sym, false)
+          true
+        elsif const_path(name)
+          true
+        else
+          false
+        end
+      end
+
+      # Public: Source location that defined the Module.
+      #
+      # Returns absolute String path under app/views.
+      attr_accessor :path
+
+      # Internal: Detect source location for constant name.
+      #
+      # name - String or Symbol constant name
+      #
+      # Examples
+      #
+      #   Views.const_path(:Users) #=> "app/views/users"
+      #   Views::Users.const_path(:Show) #=> "app/views/users/show.html.erb"
+      #   Views::Users.const_path(:Profile) #=> "app/views/users/_profile.html.erb"
+      #
+      # Returns String absolute path to file, otherwise nil.
+      def const_path(name)
+        pathname = ActiveSupport::Inflector.underscore(name.to_s)
+        Dir[File.join(self.path, "{#{pathname},_#{pathname}}{/,.*}")].map { |fn| File.expand_path(fn) }.first
+      end
+
+      # Internal: Initialize new module for constant name and load ERB statics.
+      #
+      # path - String path of directory or erb file.
+      #
+      # Examples
+      #
+      #   load_module("app/views/users")
+      #   load_module("app/views/users/show.html.erb")
+      #
+      # Returns new Module implementing Loadable concern.
+      def load_module(path)
+        mod = Module.new
+
+        if File.extname(path) == ".erb"
+          contents = File.read(path)
+          query = GraphQL::Client::Erubis.extract_graphql_sections(contents)
+          mod = client.parse(query) if query
+        end
+
+        mod.extend(ViewModule)
+        mod.client = self.client
+        mod.path = path
+        mod
+      end
+
+      # Public: Implement constant missing hook to autoload View ERB statics.
+      #
+      # name - String or Symbol constant name
+      #
+      # Returns module or raises NameError if missing.
+      def const_missing(name)
+        if path = const_path(name)
+          mod = load_module(path)
+          const_set(name, mod)
+          mod.unloadable
+          mod
+        else
+          super
+        end
+      end
+    end
+  end
+end
---
+++ test/test_view_module.rb
@@ -0,0 +1,46 @@
+require "graphql"
+require "graphql/client"
+require "graphql/client/view_module"
+require "minitest/autorun"
+
+class TestViewModule < MiniTest::Test
+  Root = File.expand_path("..", __FILE__)
+  Client = GraphQL::Client.new
+
+  module Views
+    extend GraphQL::Client::ViewModule
+    self.path = "#{Root}/views"
+    self.client = Client
+  end
+
+  def test_valid_constant_name
+    assert GraphQL::Client::ViewModule.valid_constant_name?("Foo")
+    refute GraphQL::Client::ViewModule.valid_constant_name?("404")
+  end
+
+  def test_const_path
+    assert_equal "#{Root}/views/users", Views.const_path(:Users)
+    assert_equal "#{Root}/views/users/show.html.erb", Views::Users.const_path(:Show)
+    assert_equal "#{Root}/views/users/_profile.html.erb", Views::Users.const_path(:Profile)
+
+    assert_equal nil, Views.const_path(:Missing)
+  end
+
+  def test_const_missing
+    assert_kind_of Module, Views::Users
+    assert_equal "#{Root}/views/users", Views::Users.path
+
+    assert_kind_of Module, Views::Users::Show
+    assert_equal "#{Root}/views/users/show.html.erb", Views::Users::Show.path
+
+    assert_kind_of GraphQL::Client::FragmentDefinition, Views::Users::Show::User
+    assert_equal(<<-'GRAPHQL'.gsub("      ", "").chomp, Views::Users::Show::User.document.to_query_string)
+      fragment TestViewModule__Views__Users__Show__User on User {
+        login
+      }
+    GRAPHQL
+
+    assert_kind_of Module, Views::Users::Profile
+    assert_equal "#{Root}/views/users/_profile.html.erb", Views::Users::Profile.path
+  end
+end
