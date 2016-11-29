d96e75d2cfffead8f2822f94136bc5c2272e1699
Joshua Peek <josh@joshpeek.com>
2016-11-29 23:12:32 +1100

    Document type analyzer

--- lib/graphql/client/document_types.rb
+++ lib/graphql/client/document_types.rb
@@ -2,11 +2,21 @@ require "graphql"
 
 module GraphQL
   class Client
-    # Public: Document type analyzer.
+    # Internal: Use schema to detect definition and field types.
     module DocumentTypes
+      # Internal: Detect all types used in a given document
+      #
+      # schema - A GraphQL::Schema
+      # document - A GraphQL::Language::Nodes::Document to scan
+      #
+      # Returns a Hash[Language::Nodes::Node] to GraphQL::Type objects.
       def self.analyze_types(schema, document)
         unless schema.is_a?(GraphQL::Schema)
           raise TypeError, "expected schema to be a GraphQL::Schema, but was #{schema.class}"
+        end
+
+        unless document.is_a?(GraphQL::Language::Nodes::Document)
+          raise TypeError, "expected schema to be a GraphQL::Language::Nodes::Document, but was #{document.class}"
         end
 
         visitor = GraphQL::Language::Visitor.new(document)
