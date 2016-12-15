6c43d07f1cdf2980cbd70f18ba1331560fd8ae66
Joshua Peek <josh@joshpeek.com>
2016-12-14 17:06:49 -0800

    Revert changes to error

--- lib/graphql/client/error.rb
+++ lib/graphql/client/error.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 module GraphQL
   class Client
-    # Public: Abstract base class for errors raised by GraphQL::Client.
-    class Error < ::StandardError
+    # Public: Abstract base class for all errors raised by GraphQL::Client.
+    class Error < StandardError
     end
   end
 end
