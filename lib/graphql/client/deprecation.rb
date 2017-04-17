7a4c099e30e4d5bc5291a654a4a738704f2ccdaa
Joshua Peek <josh@joshpeek.com>
2017-04-17 13:56:23 -0700

    Stub new AS deprecation methods

--- lib/graphql/client/deprecation.rb
+++ lib/graphql/client/deprecation.rb
@@ -6,7 +6,25 @@ module GraphQL
     if ActiveSupport::Deprecation.is_a?(Class)
       Deprecation = ActiveSupport::Deprecation.new("0.9", "graphql-client")
     else
-      Deprecation = ActiveSupport::Deprecation
+      module Deprecation
+        extend self
+
+        def silence(&block)
+          ActiveSupport::Deprecation.silence(&block)
+        end
+
+        def warn(*args)
+          ActiveSupport::Deprecation.warn(*args)
+        end
+
+        def deprecate_methods(*args)
+          # TODO
+        end
+
+        def deprecation_warning(deprecated_method_name, message = nil, caller_backtrace = nil)
+          warn "#{deprecated_method_name} is deprecated and will be removed from graphql-client 0.9 (#{message})"
+        end
+      end
     end
   end
 end
