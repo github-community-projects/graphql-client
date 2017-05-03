e3288b1761c559d9b9a0ae1a278ca55b6a555afb
Joshua Peek <josh@joshpeek.com>
2017-05-03 13:02:04 +0200

    Deprecate constructing fragment wrapper with hash

--- lib/graphql/client/fragment_definition.rb
+++ lib/graphql/client/fragment_definition.rb
@@ -1,11 +1,19 @@
 # frozen_string_literal: true
 
 require "graphql/client/definition"
+require "graphql/client/deprecation"
 
 module GraphQL
   class Client
     # Specific fragment definition subtype.
     class FragmentDefinition < Definition
+      def new(obj, *args)
+        if obj.is_a?(Hash)
+          GraphQL::Client::Deprecation.warn("constructing fragment wrapper from Hash is deprecated")
+        end
+
+        super
+      end
     end
   end
 end
