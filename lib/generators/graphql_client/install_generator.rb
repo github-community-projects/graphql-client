de79ef2b6ed1287d428b2148b8c6a6491dd5181b
Garrett Bjerkhoel <me@garrettbjerkhoel.com>
2018-04-26 20:43:33 -0400

    Add generator to get app setup

---
+++ lib/generators/graphql_client/install_generator.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'rails/generators/base'
+
+module GraphqlClient
+    class InstallGenerator < Rails::Generators::Base
+      desc "Install GraphQL::Client boilerplate code"
+      source_root File.expand_path('../templates', __FILE__)
+
+      class_option :schema,
+        type: :string,
+        default: nil,
+        desc: "Name for the schema constant (default: {app_name}Schema)"
+
+      def install
+        template("initializer.erb", "config/initializers/graphql_client.rb")
+      end
+
+      private
+
+      def app_name
+        Rails.application.class.parent_name
+      end
+
+      def schema_name
+        @schema_name ||= begin
+          if options[:schema]
+            options[:schema]
+          else
+            require File.expand_path("config/application", destination_root)
+            "#{app_name}Schema"
+          end
+        end
+      end
+    end
+end
