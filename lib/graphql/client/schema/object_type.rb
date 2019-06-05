7a8e3842a1efafddf699b2866f80b5ef8886eb40
John Hawthorn <john@hawthorn.email>
2019-06-04 22:18:20 -0700

    Don't define classes at all

--- lib/graphql/client/schema/object_type.rb
+++ lib/graphql/client/schema/object_type.rb
@@ -108,56 +108,7 @@ module GraphQL
 
           spreads = definition.indexes[:spreads][ast_nodes.first]
 
-          klass = Class.new(defined_class)
-          klass.define_fields(field_classes)
-          klass.instance_variable_set(:@source_definition, definition)
-          klass.instance_variable_set(:@_spreads, spreads)
-
-          if definition.client.enforce_collocated_callers
-            keys = field_classes.keys.map { |key| ActiveSupport::Inflector.underscore(key) }
-            Client.enforce_collocated_callers(klass, keys, definition.source_location[0])
-          end
-
           WithDefinition.new(defined_class, type, fields, field_classes, definition, spreads)
-        end
-
-        PREDICATE_CACHE = Hash.new { |h, name|
-          h[name] = -> { @data[name] ? true : false }
-        }
-
-        METHOD_CACHE = Hash.new { |h, key|
-          h[key] = -> {
-            name = key.to_s
-            type = self.class::FIELDS[key]
-            @casted_data.fetch(name) do
-              @casted_data[name] = type.cast(@data[name], @errors.filter_by_path(name))
-            end
-          }
-        }
-
-        MODULE_CACHE = Hash.new do |h, fields|
-          h[fields] = Module.new do
-            fields.each do |name|
-              GraphQL::Client::Schema::ObjectType.define_cached_field(name, self)
-            end
-          end
-        end
-
-        FIELDS_CACHE = Hash.new { |h, k| h[k] = k }
-
-        def define_fields(fields)
-          const_set :FIELDS, FIELDS_CACHE[fields]
-          mod = MODULE_CACHE[fields.keys.sort]
-          include mod
-        end
-
-        def self.define_cached_field(name, ctx)
-          key = name
-          name = -name.to_s
-          method_name = ActiveSupport::Inflector.underscore(name)
-
-          ctx.send(:define_method, method_name, &METHOD_CACHE[key])
-          ctx.send(:define_method, "#{method_name}?", &PREDICATE_CACHE[name])
         end
 
         def define_field(name, type)
