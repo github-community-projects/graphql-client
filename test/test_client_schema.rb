e474f1281b70b2ff5720742b1250167e6cb32982
Tabby Cromarty <tcromarty@gocardless.com>
2025-02-21 15:01:39 +0000

    Allow loading schema from SDL definition file as well as JSON
    If passing a path to a file with a name that ends in ".graphql" or
    ".graphqls" (matching the filenames used in
    [graphql-ruby](https://github.com/rmosolgo/graphql-ruby/blob/master/lib/graphql/schema.rb#L116)),
    attempt to load the schema from that file assuming it's a schema
    definition rather than a JSON schema.

--- test/test_client_schema.rb
+++ test/test_client_schema.rb
@@ -42,6 +42,22 @@ class TestClientSchema < Minitest::Test
     assert_equal "AwesomeQuery", schema.query.graphql_name
   end
 
+  def test_load_schema_from_definition_string
+    definition = Schema.to_definition
+    schema = GraphQL::Client.load_schema(definition)
+    assert_equal "AwesomeQuery", schema.query.graphql_name
+  end
+
+  def test_load_schema_from_definition_file
+    definition = Schema.to_definition
+    Tempfile.create(["schema", ".graphql"]) do |file|
+      file.write(definition)
+      file.close
+      schema = GraphQL::Client.load_schema(file.path)
+      assert_equal "AwesomeQuery", schema.query.graphql_name
+    end
+  end
+
   def test_load_schema_ignores_missing_path
     refute GraphQL::Client.load_schema("#{__dir__}/missing-schema.json")
   end
