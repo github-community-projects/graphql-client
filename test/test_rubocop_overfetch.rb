4ef6ee15e538138e2bf23752a9dcf7e13305c6a2
jmeridth <jmeridth@gmail.com>
2025-05-28 21:34:33 -0500

    Revert "fix: tests"
    This reverts commit 6277fcf5078a794103bd05437c2a48656bb6db72.

--- test/test_rubocop_overfetch.rb
+++ test/test_rubocop_overfetch.rb
@@ -12,24 +12,24 @@ class TestRubocopOverfetch < Minitest::Test
   end
 
   def test_all_fields_used
-    result = investigate(@cop, "#{Root}/views/users/show.html.erb")
+    investigate(@cop, "#{Root}/views/users/show.html.erb")
 
-    assert_empty result.offenses.map(&:message)
+    assert_empty @cop.offenses.map(&:message)
   end
 
   def test_all_fields_used_with_safe_navigation_operator
     skip if RUBY_VERSION < "2.3"
 
-    result = investigate(@cop, "#{Root}/views/users/show-2-3.html.erb")
+    investigate(@cop, "#{Root}/views/users/show-2-3.html.erb")
 
-    assert_empty result.offenses.map(&:message)
+    assert_empty @cop.offenses.map(&:message)
   end
 
   def test_field_unused
-    result = investigate(@cop, "#{Root}/views/users/overfetch.html.erb")
+    investigate(@cop, "#{Root}/views/users/overfetch.html.erb")
 
-    assert_equal 1, result.offenses.count
-    assert_equal "GraphQL/Overfetch: GraphQL field 'birthday' query but was not used in template.", result.offenses.first.message
+    assert_equal 1, @cop.offenses.count
+    assert_equal "GraphQL/Overfetch: GraphQL field 'birthday' query but was not used in template.", @cop.offenses.first.message
   end
 
   private
@@ -37,7 +37,8 @@ class TestRubocopOverfetch < Minitest::Test
   def investigate(cop, path)
     engine = GraphQL::Client::ERB.new(File.read(path))
     processed_source = RuboCop::ProcessedSource.new(engine.src.dup, RUBY_VERSION.to_f, path)
-    commissioner = RuboCop::Cop::Commissioner.new([cop], [cop], raise_error: true)
+    commissioner = RuboCop::Cop::Commissioner.new([cop], [], raise_error: true)
     commissioner.investigate(processed_source)
+    commissioner
   end
 end
